a8cd025d1b64b98faa05b25d4236b65546a8a924
‘Gonzo <gonzonieto@gmail.com>
2022-04-13 22:22:28 +0000

    delete blank lines

--- server.js
+++ server.js
@@ -17,9 +17,6 @@ const cookieSession = require("cookie-session");
 // PG database client/connection setup
 const database = require("./lib/db.js");
 
-
-
-
 // Load the logger first so all (static) HTTP requests are logged to STDOUT
 // 'dev' = Concise output colored by response status for development use.
 //         The :status token will be colored red for server error codes, yellow for client error codes, cyan for redirection codes, and uncolored for all other codes.
