5a0431421d2904ad4dc26c2a13c31fc8426049dc
Julia Tran <juliatran@Julias-MacBook-Air.local>
2022-04-13 11:28:21 -0400

    Make Order counter dynamic

--- db/seeds/04_order_items_seeds.sql
+++ db/seeds/04_order_items_seeds.sql
@@ -3,4 +3,5 @@
 INSERT INTO order_items (order_id, item)
 VALUES ('1', 'Pizza, burger'),
 ('2', 'Pizza, burger'),
-('3', 'Pizza, burger');
+('3', 'Pizza, burger'),
+('4', 'Pizza, burger');
--- db/seeds/05_orders_seeds.sql
+++ db/seeds/05_orders_seeds.sql
@@ -2,4 +2,5 @@
 INSERT INTO orders (user_id, comments, created_at)
 VALUES ('1', 'no sauce', now()),
 ('2', 'no dressing', now()),
-('3', 'no pickles', now());
+('3', 'no pickles', now()),
+('1', 'salad', now());
