8c117af312981b95d75e1f9afa89b3b2e42fe5d6
‘Gonzo <gonzonieto@gmail.com>
2022-04-13 22:22:08 +0000

    write GET /menu routing

--- routes/menu.js
+++ routes/menu.js
@@ -2,8 +2,17 @@ const express = require('express');
 const router = express.Router();
 
 module.exports = (db) => {
+
   router.get("/", (req, res) => {
-    res.render('menu');
+    db.getAllMenuItems()
+      .then((menuItems) => {
+        // or maybe res.json() ?
+        res.send(menuItems);
+      })
+      .catch(e => {
+        console.error(e);
+        res.send(e);
+      });
   });
   return router;
 };
