b8a010097c7d6b41a46bff0ab567f5e83be9d710
‘Gonzo <gonzonieto@gmail.com>
2022-04-14 00:31:05 +0000

    replicate rough structure of Gonzo's Tweeter for getting databse info to the front-end with jQuery

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,42 +1,40 @@
 $(document).ready(function() {
-  console.log("ready!");
 
-  const loadMenuItems = () => {
-    $.ajax('/menu/', { method: 'GET' })
-      .then((data) => {
-        console.log(data);
-      });
+  const loadMenu = () => {
+    $.ajax('/menu', { method: 'GET' })
+      .then((menuItems) => renderMenu(menuItems))
+      .catch(err => console.error('query error', err.stack));
+  };
+  loadMenu();
+
+  const createMenuElement = (menuItem) => {
+    // FOR DEVELOPMENT: LIST OF THE KEYS OF menuItem
+    // id,name,description,price,modifiers,photo,category,type,active
+
+    // TODO -- consider if there is a more appropriate solution to the URL issue than slicing it
+    //
+
+    const $menuItem = $(`
+      <img src="${menuItem.photo.slice(4)}" class="card-img-top" alt="Food pic" style="width:250px">
+      <div class="card-body">
+        <h5 class="card-title">${menuItem.name}</h5>
+        <p class="card-text">${menuItem.description} </p>
+        <div class="icon-row">
+          <i class="fa-regular fa-salad"></i>
+          <i class="fa-regular fa-pepper-hot"></i>
+        </div>
+        <div class="d-grid gap-2">
+          <button type="button" class="btn btn-outline-dark btn-sm">Add to Cart</button>
+        </div>
+      </div>
+        `);
+
+    return $menuItem;
   };
 
-  loadMenuItems();
-
-//   const renderMenuItems = function (items) {
-//     const $menuContainer = $('.menu-category');
-//     $menuContainer.empty();
-//     for (let i = 0; i < 2; i++) {
-//       let $item = createMenuElement(items[i]);
-//       $menuContainer.prepend($item);
-//     }
-
-//   };
-
-//   const createMenuElement = function (items) {
-
-//     const MenuCardHTML = `
-// <img src="https://via.placeholder.com/250x180.png?text=Food" class="card-img-top" alt="Food pic">
-// <div class="card-body">
-//   <h5 class="card-title">${items.name}</h5>
-//   <p class="card-text">${items.description} </p>
-//   <div class="icon-row">
-//     <i class="fa-regular fa-salad"></i>
-//     <i class="fa-regular fa-pepper-hot"></i>
-//   </div>
-//   <div class="d-grid gap-2">
-//     <button type="button" class="btn btn-outline-dark btn-sm">Add to Cart</button>
-//   </div>
-// </div>
-//     `;
-
-//     return MenuCardHTML;
-//   };
+  const renderMenu = (menuItems) => {
+    menuItems
+      .map(menuItemData => createMenuElement(menuItemData))
+      .forEach(menuElement => $('#menu').prepend(menuElement));
+  };
 });
