b6a0eb3e6ef11448e520e7c57e2c33d65ac25afe
Julia Tran <juliatran@Julias-MacBook-Air.local>
2022-04-14 23:35:20 -0400

    Styling changes

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -14,7 +14,7 @@ $(document).ready(() => {
     // TODO -- consider if there is a more appropriate solution to the URL issue than slicing it
 
     const $menuItem = $(`
-      <img src="${menuItem.photo.slice(4)}" class="card-img-top" alt="Food pic" style="width:250px">
+      <img src="${menuItem.photo}" class="card-img-top" alt="Food pic" style="width:250px">
       <div class="card-body">
         <h5 class="card-title">${menuItem.name}</h5>
         <p class="card-text">${menuItem.description} </p>
