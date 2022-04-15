ed95be2d66866785c768ca2e65ea1a6749a05b95
‘Gonzo <gonzonieto@gmail.com>
2022-04-15 04:54:43 +0000

    add logging for development

--- public/scripts/cart.js
+++ public/scripts/cart.js
@@ -3,6 +3,13 @@
 // Cart keeps an array of the menu items that have been added to cart
 
 $().ready(() => {
+  const cart = [];
+
   // This selects all the add to cart buttons across the menu item cards
-  $('button.add-to-cart');
+  $('button.add-to-cart').click(() => {
+
+  });
+
+  console.log('Logging contents of cart:');
+  console.log(cart);
 });
