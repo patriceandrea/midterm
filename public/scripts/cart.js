1ae5fd745e78df9ab669853dc9e8ad0db353b786
‘Gonzo <gonzonieto@gmail.com>
2022-04-15 08:58:26 +0000

    Revert "lint and remove old comments"
    This reverts commit 1b35c7cdf253c658ef423126dda0f7c4818f395c.

--- public/scripts/cart.js
+++ public/scripts/cart.js
@@ -1,7 +1,11 @@
-$().ready(() => {
+/////////////////
+// IN DEVELOPMENT
+// Cart keeps an array of the menu items that have been added to cart
+
+$(document).ready(() => {
   const cart = [];
 
-  $().on('click', '.add-to-cart', (event) => {
+  $(document).on('click', '.add-to-cart', (event) => {
     cart.push(event.target.id);
     alert(`You selected menu item ${event.target.id}!
     Items in cart: ${cart}`);
