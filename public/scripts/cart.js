ed07ea21193d19869f5801c8399893182747d720
‘Gonzo <gonzonieto@gmail.com>
2022-04-15 06:01:05 +0000

    change to simpler logging msg

--- public/scripts/cart.js
+++ public/scripts/cart.js
@@ -7,8 +7,8 @@ $(document).ready(() => {
 
   $(document).on('click', '.add-to-cart', (event) => {
     cart.push(event.target.id);
-    alert(`You selected menu item ${event.target.id}! The cart now has the following items in it: ${cart}`);
-
+    alert(`You selected menu item ${event.target.id}!
+    Items in cart: ${cart}`);
   });
 });
 
