45bdab56ed9ef24a8e68b75655e0d05785daae0c
Julia Tran <juliatran@Julias-MacBook-Air.local>
2022-04-14 21:00:47 -0400

    Delete customer login page

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,47 +1,47 @@
 // Client facing scripts here
-// $(document).ready(function () {
-//   console.log("ready!");
+$(document).ready(function () {
+  console.log("ready!");
 
-//   loadMenuItems();
-
-
-//   const renderMenuItems = function (items) {
-//     const $menuContainer = $('.menu-category');
-//     $menuContainer.empty();
-//     for (let i = 0; i < 2; i++) {
-//       let $item = createMenuElement(items[i]);
-//       $menuContainer.prepend($item);
-//     }
-
-//   };
-
-//   const createMenuElement = function (items) {
-
-//     const MenuCardHTML = `
-// <img src="https://via.placeholder.com/250x180.png?text=Food" class="card-img-top" alt="Food pic">
-// <div class="card-body">
-//   <h5 class="card-title">${items.name}</h5>
-//   <p class="card-text">${items.description} </p>
-//   <div class="icon-row">
-//     <i class="fa-regular fa-salad"></i>
-//     <i class="fa-regular fa-pepper-hot"></i>
-//   </div>
-//   <div class="d-grid gap-2">
-//     <button type="button" class="btn btn-outline-dark btn-sm">Add to Cart</button>
-//   </div>
-// </div>
-//     `;
-
-//     return MenuCardHTML;
-//   };
+  loadMenuItems();
 
 
-//   function loadMenuItems() {
-//     $.ajax('/', { method: 'GET' })
-//       .then(function (data) {
-//         console.log('Success! ');
-//         // $('#tweet-text').val('');
-//         renderMenuItems(data);
-//       });
-//   };
-// });
+  const renderMenuItems = function (items) {
+    const $menuContainer = $('.menu-category');
+    $menuContainer.empty();
+    for (let i = 0; i < 2; i++) {
+      let $item = createMenuElement(items[i]);
+      $menuContainer.prepend($item);
+    }
+
+  };
+
+  const createMenuElement = function (items) {
+
+    const MenuCardHTML = `
+<img src="https://via.placeholder.com/250x180.png?text=Food" class="card-img-top" alt="Food pic">
+<div class="card-body">
+  <h5 class="card-title">${items.name}</h5>
+  <p class="card-text">${items.description} </p>
+  <div class="icon-row">
+    <i class="fa-regular fa-salad"></i>
+    <i class="fa-regular fa-pepper-hot"></i>
+  </div>
+  <div class="d-grid gap-2">
+    <button type="button" class="btn btn-outline-dark btn-sm">Add to Cart</button>
+  </div>
+</div>
+    `;
+
+    return MenuCardHTML;
+  };
+
+
+  function loadMenuItems() {
+    $.ajax('/', { method: 'GET' })
+      .then(function (data) {
+        console.log('Success! ');
+        // $('#tweet-text').val('');
+        renderMenuItems(data);
+      });
+  };
+});
