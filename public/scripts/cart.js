2d9201e579a6e18c988c6eb5382c682baf54c6b7
Gonzo Nieto <5186441+gonzonieto@users.noreply.github.com>
2022-04-15 05:53:54 -0400

    send menu item IDs to /cart via URL parameter

--- public/scripts/cart.js
+++ public/scripts/cart.js
@@ -9,7 +9,7 @@ $().ready(() => {
 
   $('#cartIcon').on('click', (event) => {
     event.preventDefault();
-    window.location = 'http://localhost:8080/cart&items=' + cart.join();
+    window.location = 'http://localhost:8080/cart?items=' + cart.join();
   });
 });
 
--- routes/cart.js
+++ routes/cart.js
@@ -1,10 +1,17 @@
-
 const express = require('express');
 const router = express.Router();
 const {sendRestaurantMessage} = require('../twilio.js');
 
 module.exports = () => {
   router.get("/", (req, res) => {
+    const menuItems = req.query;
+    console.log('menuItems');
+    console.log(menuItems.items.split(',').map(x => parseInt(x)));
+    res.render("cart", menuItems);
+  });
+
+  router.get("/?:items/", (req, res) => {
+
     res.render("cart");
   });
 
