bf5605905bad9c5389f09089e50e30998037530c
Julia Tran <juliatran@Julias-MacBook-Air.local>
2022-04-14 20:47:44 -0400

    Remove twilio in admin route

--- routes/admin.js
+++ routes/admin.js
@@ -1,31 +1,13 @@
 const express = require('express');
 const router = express.Router();
+const { sendCustomerMessage } = require("../twilio.js");
 
-require("dotenv").config();
-const accountSid = process.env.TWILIO_ACCOUNT_SID;
-const authToken= process.env.TWILIO_AUTH_TOKEN;
-const twilio = require('twilio'); // npm i twilio
-const client = new twilio(accountSid, authToken);
-
-const sendCustomerMessage = (name, phone) => {
-  console.log("sending message...", "name", name, "phone", phone);
-  client.messages
-    .create({
-      body: `Hi ${name},Your order is ready! Thank you for ordering with us.`,
-      from: process.env.TWILIO_NUMBER,
-      to: phone
-    })
-    .then(message => console.log("message id: " + message.sid))
-    .catch(err => {
-      console.log(err);
-    });
-};
 
 module.exports = (db) => {
   router.get("/", (req, res) => {
     db.getAllPendingOrders()
-    .then((orderItems) =>  res.render("admin", {orderItems: orderItems}))
-    .catch((e) => res.send(e));
+      .then((orderItems) => res.render("admin", { orderItems: orderItems }))
+      .catch((e) => res.send(e));
 
   })
 
@@ -33,17 +15,17 @@ module.exports = (db) => {
 
 
   router.post("/complete", (req, res) => {
-    console.log("Request",req.body);
-    const  {customer, phone, completeOrderId} = req.body
+  
+    const { customer, phone, completeOrderId } = req.body
     db.setOrderCompleteWithOrderId(completeOrderId).then(() => {
       db.getAllPendingOrders()
-    .then((orderItems) => {
+        .then((orderItems) => {
 
-      sendCustomerMessage(customer, phone);
-      return res.render("admin", {orderItems: orderItems})
+          sendCustomerMessage(customer, phone);
+          return res.render("admin", { orderItems: orderItems })
 
 
-    })
+        })
     })
       .catch(e => res.send(e));
   });
