9841c45e1aa25ab9d028134e684f03fc76ce78be
‘Gonzo <gonzonieto@gmail.com>
2022-04-15 04:48:11 +0000

    add jQuery script template with add-to-cart button selector

--- public/scripts/cart.js
+++ public/scripts/cart.js
@@ -1,1 +1,8 @@
-$().ready(() => {});
+/////////////////
+// IN DEVELOPMENT
+// Cart keeps an array of the menu items that have been added to cart
+
+$().ready(() => {
+  // This selects all the add to cart buttons across the menu item cards
+  $('button.add-to-cart');
+});
