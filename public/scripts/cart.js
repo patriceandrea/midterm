77e92ad9e665df72f576a4e439ee601806f856b9
‘Gonzo <gonzonieto@gmail.com>
2022-04-15 05:51:28 +0000

    cart now collects all menu items added to it

--- public/scripts/cart.js
+++ public/scripts/cart.js
@@ -2,14 +2,13 @@
 // IN DEVELOPMENT
 // Cart keeps an array of the menu items that have been added to cart
 
-$().ready(() => {
+$(document).ready(() => {
   const cart = [];
 
-  // This selects all the add to cart buttons across the menu item cards
-  $('button.add-to-cart').click(() => {
+  $(document).on('click', '.add-to-cart', (event) => {
+    cart.push(event.target.id);
+    alert(`You selected menu item ${event.target.id}! The cart now has the following items in it: ${cart}`);
 
   });
-
-  console.log('Logging contents of cart:');
-  console.log(cart);
 });
+
